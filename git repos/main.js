5d30d1f9dc3c8c671b87137e88e26c1a23453fef
Mostafa-Ashraff <94311378+Mostafa-Ashraff@users.noreply.github.com>
2022-11-04 22:00:58 +0200

    protfolio changes

--- git repos/main.js
+++ git repos/main.js
@@ -6,6 +6,7 @@ searchBtn.onclick =function() {
 };
 function getRepos(){
     if(theInput.value !== null && theInput.value !== ''){
+    reposDiv.innerHTML='';        
     fetch(`https://api.github.com/users/${theInput.value}/repos`)
     .then(response => response.json())
     .then(repos => {repos.forEach(repo => {
@@ -13,21 +14,25 @@ function getRepos(){
         div.classList.add('repo')
         let para = document.createElement('p');
         para.className = 'name';
+        para.textContent = repo.name;
+        div.appendChild(para);
         let infoDiv = document.createElement('div');
         infoDiv.className = 'info';
         let forkNo = document.createElement('span');
+        forkNo.className = 'btn'
+        forkNo.textContent =` Forks: ${repo.forks_count}`
+        infoDiv.appendChild(forkNo);
+        let starsCount = document.createElement('span');
+        starsCount.className = 'btn'
+        starsCount.textContent =` Star: ${repo.stargazers_count}`
+        infoDiv.appendChild(starsCount);
         let url = document.createElement('span');
         let urlLink = document.createElement('a');
         urlLink.textContent ='visit';
         urlLink.href = `https://github.com/${theInput.value}/${repo.name}`;
         urlLink.setAttribute('target', '_blank');
-        url.appendChild(urlLink);
         url.className='btn'
-        forkNo.className = 'btn'
-        para.textContent = repo.name;
-        forkNo.textContent =` Forks: ${repo.forks_count}`
-        div.appendChild(para);
-        infoDiv.appendChild(forkNo);
+        url.appendChild(urlLink);
         infoDiv.appendChild(url);
         div.appendChild(infoDiv);
         reposDiv.appendChild(div); 
